3f514dc35cd7a0195a3a46289263a193bb6045e3
opilane <opilane@HP-CZC6297B8Z>
2021-11-23 16:37:00 +0200

    Add project files.

---
+++ KontrolRab.py
@@ -0,0 +1,40 @@
+
+#task1
+#skvor=""
+#for s in range(1,9):
+	#A=int(input("skolko skvoreshnikov: "))
+	#if A==1:
+
+#task2
+#for i in range(100):
+	#a=int(input("Enter Number"))
+	#b=int(input("Enter stepen"))
+	#if
+
+#task3
+from random import *
+from math import *
+#students=randint(1,30)
+#A=0
+#for i in range(students):
+	#mark=randint(1,5)
+#task4
+
+#task5
+#for stroka in range(1,9):
+	#if stroka==1:
+		#print("1",end=" ")
+	#elif stroka==2:
+		#print("1",end=" ")
+	#elif stroka==3:
+		#print("2",end=" ")
+	#elif stroka==4:
+		#print("3",end=" ")
+	#elif stroka==5:
+		#print("5",end=" ")
+	#elif stroka==6:
+		#print("8",end=" ")
+	#elif stroka==7:
+		#print("13",end=" ")
+	#elif stroka==8:
+		#print("21",end=" ")
